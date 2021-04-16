437337fe3b79dc39e443790f32ac9e13f409a48f
Sam <stormer101.sw@gmail.com>
2021-04-16 21:52:59 +0100

    improved navbar

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+var pages = ['home', 'about', 'gallery', 'contact']
+
 function loadNavBar() {
+    document.getElementById('home').style.display = 'block';
     $(document).ready(function() {
         $.each($("nav li a img"), function(i, link) {
             $(link).fadeIn(2200);
         });
+        
     });
 };
+
+
+function switch_page(page){
+    document.getElementById(page).style.display = 'block';
+}
